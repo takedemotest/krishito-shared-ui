7f9139e5b41d8fd5aafb9d8447e84330f4a282bd
takedemotest <takedemotest@gmail.com>
2026-07-29 18:43:24 +0530

    build file

---
+++ dist/shared-ui/fesm2022/shared-ui.mjs
@@ -0,0 +1,22 @@
+import * as i0 from '@angular/core';
+import { Component } from '@angular/core';
+
+class SharedUi {
+    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "22.0.8", ngImport: i0, type: SharedUi, deps: [], target: i0.ɵɵFactoryTarget.Component });
+    static ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "22.0.8", type: SharedUi, isStandalone: true, selector: "lib-shared-ui", ngImport: i0, template: ` <p>shared-ui works!</p> `, isInline: true, styles: [""] });
+}
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "22.0.8", ngImport: i0, type: SharedUi, decorators: [{
+            type: Component,
+            args: [{ selector: 'lib-shared-ui', imports: [], template: ` <p>shared-ui works!</p> ` }]
+        }] });
+
+/*
+ * Public API Surface of shared-ui
+ */
+
+/**
+ * Generated bundle index. Do not edit.
+ */
+
+export { SharedUi };
+//# sourceMappingURL=shared-ui.mjs.map
